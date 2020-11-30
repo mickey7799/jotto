1b4d7762639b9953775f096c7a1b23a34b8eac6e
mickey7799 <vickeylee.w@gmail.com>
2020-11-30 19:50:57 +1100

    Extract enzyme setup to setupTests.js

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 const defaultProps = { success: false };
 
 const setup = (props = {}) => {
-  const setupProps = { ...defaultProps };
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test('renders without error', () => {

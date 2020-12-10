c5ae7f418a43a908a73133fe408d8429c81404b4
mickey7799 <vickeylee.w@gmail.com>
2020-12-10 19:44:15 +1100

    Added tests for Input success = true

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -20,21 +20,39 @@ describe('render', () => {
       wrapper = setup(initialState);
     });
     test('renders component without error', () => {
-      const inputBox = findByTestAttr(wrapper, 'component-input');
-      expect(inputBox.length).toBe(1);
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
+    });
+    test('renders submit button', () => {
       const submitButton = findByTestAttr(wrapper, 'submit-button');
       expect(submitButton.length).toBe(1);
     });
-    test('renders submit button', () => {});
   });
 });
 
 describe('update state', () => {
   describe('word has been guessed', () => {
-    test('renders component without error', () => {});
-    test('does not render input box', () => {});
-    test('does not render submit button', () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('does not render input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
+    });
+    test('does not render submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
+    });
   });
 });

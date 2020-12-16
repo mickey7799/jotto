082334770c70439efd058806b2bc9f39beb0be09
mickey7799 <vickeylee.w@gmail.com>
2020-12-16 21:00:10 +1100

    Connect App component and test

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,4 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {});
+import App from './App';
+import { storeFactory } from '../test/testUtils';
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe('redux props', () => {
+  test('has access to `success` state', () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test('has access to `secretWord` state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const gussedWordsProp = wrapper.instance().props.guessedWords;
+    expect(gussedWordsProp).toEqual(guessedWords);
+  });
+  test('`getSecretWord` action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});

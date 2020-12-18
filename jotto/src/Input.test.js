bc544bd9f61d9b52893cb193d001e61d86d17ce5
mickey7799 <vickeylee.w@gmail.com>
2020-12-18 20:58:45 +1100

    Added guessWord action testing

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
@@ -68,5 +68,19 @@ describe('redux props', () => {
     const wrapper = setup();
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+
+describe('`guessWord` action creator call', () => {
+  test('`guessWord` runs on App mount', () => {
+    const guessWordMock = jest.fn();
+    const props = {
+      guessWord: guessWordMock
+    };
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click');
+    const guessWordMockCallCount = guessWordMock.mock.calls.length;
+    expect(guessWordMockCallCount).toBe(1);
   });
 });
